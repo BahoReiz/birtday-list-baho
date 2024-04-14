bf50f729376b071931d06aaed106a83f09dc278c
BahoReiz <bahadirkoca78@gmail.com>
2024-04-14 16:57:41 +0300

    Added current date and some styling

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React, { useState } from 'react';
 import data  from './data';
 import List from './List';
 
-
+function getDate() {
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const year = today.getFullYear();
+  const date = today.getDate();
+  return `${month}/${date}/${year}`; 
+}
 
 function App() {
   const [people, setPeople] = useState(data)
+  const [currentDate, setCurrentDate] = useState(getDate());
   return (
   <main>
     <section className="container">
-      <h2>Hello Bahadir</h2>
+      <h1>Welcome Bahadir! <br></br>
+      Today is ...
+      </h1>
+      <h3>{currentDate}</h3>
       <List people={people}/>       
       <button onClick={() => setPeople([])}>
         Clear all
